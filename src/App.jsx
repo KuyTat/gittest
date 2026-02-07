8f76e0a7dea54ad4299c010c48ddd6c7192b936b
KuyTat <kuytat64@gmail.com>
2026-02-07 22:47:15 +0700

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h2 className="text-2xl font-bold text-amber-800">hello world</h2>
+      <h2 className="text-2xl font-bold text-amber-800">
+        hello world Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam aspernatur temporibus corrupti corporis at officia, commodi quasi
+        esse rerum reprehenderit repudiandae neque culpa minus sint tempora
+        cumque similique doloremque accusantium.
+      </h2>
     </div>
   );
 };
