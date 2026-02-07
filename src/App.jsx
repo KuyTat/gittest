79f204b7e5758193b92d78f314d6fd0cc94bc10f
KuyTat <kuytat64@gmail.com>
2026-02-07 22:32:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-amber-800">hello world</h2>
+    </div>
+  );
+};
+
+export default App;
