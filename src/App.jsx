7b3b9164915d6f2d88561de9d10583bae0207c21
KuyTat <kuytat64@gmail.com>
2026-02-08 11:57:29 +0700

    update tages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h2 className="text-2xl font-bold text-amber-800">hello world</h2>
+      <h2 className="text-2xl font-bold text-amber-800">
+        what is your name? My name is Tat.
+      </h2>
+      <h1>new update</h1>
+      <h1>new update2</h1>
     </div>
   );
 };
